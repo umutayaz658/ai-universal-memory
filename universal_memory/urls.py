821a565ea17fcb10442bf71f00e988809ca18ab7
Umut Ayazoğlu <umtayz658@gmail.com>
2025-12-31 20:11:13 +0300

    Add Privacy Policy page

--- universal_memory/urls.py
+++ universal_memory/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
+    path('privacy-policy/', TemplateView.as_view(template_name="privacy_policy.html"), name='privacy-policy'),
 ]
