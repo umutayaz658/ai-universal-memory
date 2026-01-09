25e44c99ce09cfbf73f50514084f0df46f06c146
Umut Ayazoğlu <umtayz658@gmail.com>
2026-01-09 12:44:00 +0300

    Privacy Policy Contact Update.

--- universal_memory/urls.py
+++ universal_memory/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('page-security-roarkforge/', admin.site.urls),
     path('api/', include('core.urls')),
     path('privacy-policy/', TemplateView.as_view(template_name="privacy_policy.html"), name='privacy-policy'),
 ]
